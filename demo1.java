1a61e6f77d9e6bf66edf30cf1faf6c4096c42681
harinder <har263@yahoo.com>
2021-01-28 12:16:52 +0000

    added demo2.java

--- demo1.java
+++ demo1.java
@@ -1,8 +1,10 @@
 class Demo1
 {
-	public statis void main (String args[])
+	public static void main (String args[])
 	{
-		System.out.println("Java Demo Class 1")
+		System.out.println("Java Demo Class 1");
+			System.out.println("This is the second line in Demo1.java");
+			
 	}
 }
 
---
+++ demo2.java
@@ -0,0 +1,10 @@
+class Demo2
+{
+	public statis void main (String args[])
+	{
+		System.out.println("Java Demo Class 2")
+	
+			
+	}
+}
+
